c96a3fadc7a98cf86798d235768815380df24dad
Gabriele Donato <gabriele.donato@studenti.unimi.it>
2025-07-07 18:44:42 +0200

    Added eslint & favicon

--- back/index.js
+++ back/index.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.static(__dirname + '/../front')); // Tell express from where it should deliver the static content
+app.use('/favicon.ico', express.static(__dirname + '/../front/favicon.ico'));
 
 require('./modules/album.js');
 require('./modules/characters.js');
